66f586e81eff18108aaf3585ace00b4664edb16b
Tobias Klemmer <tobias@klemmer.info>
2020-11-22 19:30:50 +0100

    x

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage, GetStaticProps } from 'next'
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
 interface Props {
     content: { attributes: HomeAttributes }
@@ -9,6 +9,66 @@ interface HomeAttributes {
     hero_description: string
     hero_image: string
 }
+
+/// <reference types="react" />
+declare const VALID_LOADING_VALUES: readonly ['lazy', 'eager', undefined]
+declare type LoadingValue = typeof VALID_LOADING_VALUES[number]
+declare const VALID_LAYOUT_VALUES: readonly [
+    'fill',
+    'fixed',
+    'intrinsic',
+    'responsive',
+    undefined
+]
+declare type LayoutValue = typeof VALID_LAYOUT_VALUES[number]
+declare type ImgElementStyle = NonNullable<
+    JSX.IntrinsicElements['img']['style']
+>
+export declare type ImageProps = Omit<
+    JSX.IntrinsicElements['img'],
+    'src' | 'srcSet' | 'ref' | 'width' | 'height' | 'loading' | 'style'
+> & {
+    src: string
+    quality?: number | string
+    priority?: boolean
+    loading?: LoadingValue
+    unoptimized?: boolean
+    objectFit?: ImgElementStyle['objectFit']
+    objectPosition?: ImgElementStyle['objectPosition']
+} & (
+        | {
+              width?: never
+              height?: never
+              /** @deprecated Use `layout="fill"` instead */
+              unsized: true
+          }
+        | {
+              width?: never
+              height?: never
+              layout: 'fill'
+          }
+        | {
+              width: number | string
+              height: number | string
+              layout?: Exclude<LayoutValue, 'fill'>
+          }
+    )
+export default function Image({
+    src,
+    sizes,
+    unoptimized,
+    priority,
+    loading,
+    className,
+    quality,
+    width,
+    height,
+    objectFit,
+    objectPosition,
+    ...all
+}: ImageProps): JSX.Element
+export {}
+
 const HomePage: NextPage<Props> = ({ content }) => {
     const { attributes } = content
     return (
